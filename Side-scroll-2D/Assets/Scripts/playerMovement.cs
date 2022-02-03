e2238a2a7d3eba922c9400b9c58dfc4f33c09552
Berkay Şahin <akassnowflake@gmail.com>
2022-02-03 15:00:16 +0300

    Add Movement Script

--- Side-scroll-2D/Assets/Scripts/playerMovement.cs
+++ Side-scroll-2D/Assets/Scripts/playerMovement.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public CharacterController2D _controller;
+    private float horizontalMove = 0f;
+    public  float horizontlalSpeed; 
+    private bool jump=false; 
+    private bool crouch=false;
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * horizontlalSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
         
+        crouch = Input.GetButton("Crouch");
+        
+
+        if(crouch == true)
+        {
+            Debug.Log("crouch true");
+        }
+        else if(crouch == false)
+        {
+            Debug.Log("crouch false");
+        }
+
+        if(jump == true)
+        {
+            Debug.Log("jump true");
+        }
+        else if(jump == false)
+        {
+            Debug.Log("jump false");
+        }
+        
+    }
+    void FixedUpdate()
+    {
+        _controller.Move(horizontalMove * Time.fixedDeltaTime,crouch,jump);
+        jump = false;
     }
 }
