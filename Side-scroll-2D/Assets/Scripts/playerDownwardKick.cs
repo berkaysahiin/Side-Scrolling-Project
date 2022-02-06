22574286ceee24528d2ae1709fa376a8f816e74c
Berkay Şahin <akassnowflake@gmail.com>
2022-02-06 14:22:50 +0300

    Add New Attack

--- Side-scroll-2D/Assets/Scripts/playerDownwardKick.cs
+++ Side-scroll-2D/Assets/Scripts/playerDownwardKick.cs
@@ -4,6 +4,43 @@ using UnityEngine;
 
 public class playerDownwardKick : MonoBehaviour
 {
-    private Animator _anim;
+    private bool downkick;
+    private float cooldown;
+    public float startCooldown;
+    public playerAttackManager _pam;
+
+    void Start()
+    {
+        cooldown = startCooldown;
+        
+    }
+    void Update()
+    {
+        downkick = false;
+        cooldown -= Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.X) && !_pam._char.m_Grounded && _pam.globalCooldown <= 0 &&  cooldown <= 0 )
+        {
+            downkick = true;
+            cooldown = startCooldown;
+            _pam.globalCooldown = _pam.startGlobalCooldown;
+
+            Debug.Log("attack 4");
+
+           Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_pam.transform4.position, _pam.attackRadius ,_pam.enemyLayers);
+
+           foreach(Collider2D enemy in hitEnemies)
+           {
+               Debug.Log("4 we hit" + enemy.name);
+           }
+        }
+    _pam._anim.SetBool("downkick",downkick);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(_pam.transform4.position,_pam.attackRadius);
+    }
+
     
 }
